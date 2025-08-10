68fa160cae68bc201b220b9c11a90e1048f9674b
Morm-15 <mostafaradwanyos@gmail.com>
2025-08-10 17:00:01 +0300

    Add Home and ForgotPassword routes to AuthStackParamList

--- navigation/types.tsx
+++ navigation/types.tsx
@@ -1,7 +1,7 @@
 // navigation/types.ts
-
-// هذا تعريف لجميع الشاشات في ملاح المصادقة (AuthStack)
 export type AuthStackParamList = {
-    Login: undefined; // شاشة تسجيل الدخول لا تتوقع أي بيانات
-    Register: undefined; // شاشة إنشاء الحساب لا تتوقع أي بيانات
+    Login: undefined;
+    Register: undefined;
+    ForgotPassword: undefined;
+    Home: undefined;
 };
